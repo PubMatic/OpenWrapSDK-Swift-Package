4d4d3e3c67f9ecc0d6ab62daafd10df3518d485a
jenkins <jenkins-k8s@pubmatic.com>
2025-08-13 12:36:12 +0530

    [4.8.1] Updated package with OW SDK v4.8.1 and OM SDK v1.5.4-1

--- Package.swift
+++ Package.swift
@@ -11,13 +11,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OpenWrapSDK",
-            url: "https://repo.pubmatic.com/artifactory/public-repos/ios/swift-pkg-manager/openwrapsdk/4.8.0/openwrapsdk-4.8.0.zip",
-            checksum: "32b4806aa0b8bc998b5f482d8836e3dae8aed9885a05e4fc0d225f34ccdc25f1"
+            url: "https://repo.pubmatic.com/artifactory/public-repos/ios/swift-pkg-manager/openwrapsdk/4.8.1/openwrapsdk-4.8.1.zip",
+            checksum: "fc2c727b5f3e346dc609785db0bb8758766c94e8f7149a350f9d03ac4c1e6de6"
         ),
         .binaryTarget(
             name: "OMSDK_Pubmatic",
-            url: "https://repo.pubmatic.com/artifactory/public-repos/ios/swift-pkg-manager/omsdk-pubmatic/1.5.4/omsdk-pubmatic-1.5.4.zip",
-            checksum: "c70a36f38ceb6ed7e7a00a2ccb4a4a86137b984d6d6ad36aae50f46106cc2825"
+            url: "https://repo.pubmatic.com/artifactory/public-repos/ios/swift-pkg-manager/omsdk-pubmatic/1.5.4-1/omsdk-pubmatic-1.5.4-1.zip",
+            checksum: "4f77aec44b56481cb4a03498e1f7e2649bb2c87ba91b6a7413db1c7859795010"
         )
     ]
 )
